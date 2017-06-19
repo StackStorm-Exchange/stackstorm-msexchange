d9fd3c1fd30191f5cda87668bf872d7800b00eb9
Anthony Shaw <anthonyshaw@apache.org>
2017-06-19 12:37:45 +1000

    update the base action to store EWS resolved URLs in the ccache

--- actions/base/action.py
+++ actions/base/action.py
@@ -1,10 +1,16 @@
+from collections import namedtuple
+
 from st2actions.runners.pythonrunner import Action
+from st2client.models import KeyValuePair
 from exchangelib import Account, ServiceAccount, Configuration, DELEGATE
+
+CacheEntry = namedtuple('CacheEntry', 'ews_url ews_auth_type primary_smtp_address')
 
 
 class BaseExchangeAction(Action):
     def __init__(self, config):
         super(BaseExchangeAction, self).__init__(config)
+        self.client = self._init_client()
         self._credentials = ServiceAccount(
             username=config['username'],
             password=config['password'])
@@ -14,19 +20,58 @@ class BaseExchangeAction(Action):
             autodiscover = False
         except KeyError:
             autodiscover = True
-
-        if autodiscover:
-            self.account = Account(
-                primary_smtp_address=config['primary_smtp_address'],
-                credentials=self._credentials,
-                autodiscover=autodiscover,
-                access_type=DELEGATE)
-        else:
+        cache = self._get_cache()
+        if cache:
             config = Configuration(
-                server=server,
-                credentials=self._credentials)
+                service_endpoint=cache.ews_url,
+                credentials=self._credentials,
+                auth_type=cache.ews_auth_type)
             self.account = Account(
-                primary_smtp_address=config['primary_smtp_address'],
+                primary_smtp_address=cache.primary_smtp_address,
                 config=config,
                 autodiscover=False,
-                access_type=DELEGATE)
+                access_type=DELEGATE
+            )
+        else:
+            if autodiscover:
+                self.account = Account(
+                    primary_smtp_address=config['primary_smtp_address'],
+                    credentials=self._credentials,
+                    autodiscover=autodiscover,
+                    access_type=DELEGATE)
+            else:
+                config = Configuration(
+                    server=server,
+                    credentials=self._credentials)
+                self.account = Account(
+                    primary_smtp_address=config['primary_smtp_address'],
+                    config=config,
+                    autodiscover=False,
+                    access_type=DELEGATE)
+            self._store_cache_configuration()
+
+    def _store_cache_configuration(self):
+        ews_url = self.account.protocol.service_endpoint
+        ews_auth_type = self.account.protocol.auth_type
+        primary_smtp_address = self.account.primary_smtp_address
+        self.client.keys.update(KeyValuePair(name='exchange_ews_url',
+            value=ews_url))
+        self.client.keys.update(KeyValuePair(name='exchange_ews_auth_type',
+            value=ews_auth_type))
+        self.client.keys.update(KeyValuePair(name='exchange_primary_smtp_address',
+            value=primary_smtp_address))
+
+    def _get_cache(self):
+        ews_url = self.client.keys.get_by_name(
+            name='exchange_ews_url')
+        ews_auth_type = self.client.keys.get_by_name(
+            name='exchange_ews_auth_type')
+        primary_smtp_address = self.client.keys.get_by_name(
+            name='exchange_primary_smtp_address')
+        if ews_url:
+            return CacheEntry(
+                ews_url=ews_url.value,
+                ews_auth_type=ews_auth_type.value,
+                primary_smtp_address=primary_smtp_address.value)
+        else:
+            return None
