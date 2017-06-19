174c37340dc5a5118b651aeea6dfdaa696511e4e
Anthony Shaw <anthonyshaw@apache.org>
2017-06-19 14:10:42 +1000

    switch the filter dates

--- actions/get_calendar_items.py
+++ actions/get_calendar_items.py
@@ -5,10 +5,11 @@ from exchangelib import EWSDateTime
 class GetCalendarItems(BaseExchangeAction):
     def run(self, start_year, start_day, start_month, end_year, end_day, end_month):
         items = self.account.calendar.filter(
-            start__lt=self.timezone.localize(EWSDateTime(start_year, start_month, start_day)),
-            end__gt=self.timezone.localize(EWSDateTime(end_year, end_month, end_day)),
+            start__lt=self.timezone.localize(EWSDateTime(end_year, end_month, end_day)),
+            end__gt=self.timezone.localize(EWSDateTime(start_year, start_month, start_day)),
         )
         results = []
+        self.logger.info('Found {0} calendar entries'.format(len(items)))
         for item in items:
             results.append({
                 'start': item.start,
