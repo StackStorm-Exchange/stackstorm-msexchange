569226a04469423c4a7370d35996642d6e88b9f7
Anthony Shaw <anthonyshaw@apache.org>
2017-06-19 21:05:54 +1000

    use the correct filter

--- sensors/item_sensor.py
+++ sensors/item_sensor.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from st2reactor.sensor.base import PollingSensor
-from exchangelib import Account, ServiceAccount, Configuration, DELEGATE, EWSDateTime
+from exchangelib import Account, ServiceAccount, Configuration, DELEGATE, EWSDateTime, EWSTimeZone
 
 
 class ItemSensor(PollingSensor):
@@ -10,6 +10,7 @@ class ItemSensor(PollingSensor):
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
         self._stop = False
         self._store_key = 'exchange.item_sensor_date_str'
+        self._timezone = EWSTimeZone.timezone(config['timezone'])
         self._credentials = ServiceAccount(
             username=config['username'],
             password=config['password'])
@@ -43,7 +44,7 @@ class ItemSensor(PollingSensor):
         if not stored_date:
             stored_date = datetime.now()
         start_date = EWSDateTime.from_datetime(stored_date)
-        items = self.account.root.get_folder_by_name(self.sensor_folder).filter(is__read=False).filter(start__gt=start_date).values('item_id', 'subject', 'body', 'datetime_received')
+        items = self.account.root.get_folder_by_name(self.sensor_folder).filter(is__read=False).filter(datetime_received__gt=start_date).values('item_id', 'subject', 'body', 'datetime_received')
         self._logger.info("Found {0} items".format(len(items)))
         for item in items:
             self._sensor_service.dispatch(trigger='exchange_new_item', payload=item)
