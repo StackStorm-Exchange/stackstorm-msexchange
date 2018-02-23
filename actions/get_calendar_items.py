14aa41636bc033bad7159be1c7d0a7fbfad37979
Lindsay Hill <lindsay.k.hill@gmail.com>
2018-02-22 19:13:44 -0800

    linting

--- actions/get_calendar_items.py
+++ actions/get_calendar_items.py
@@ -4,10 +4,10 @@ from exchangelib import EWSDateTime
 
 class GetCalendarItems(BaseExchangeAction):
     def run(self, start_year, start_day, start_month, end_year, end_day, end_month):
-        items = self.account.calendar.filter( 
+        items = self.account.calendar.filter(
             start__lt=self.timezone.localize(EWSDateTime(end_year, end_month, end_day)),
             end__gt=self.timezone.localize(EWSDateTime(start_year, start_month, start_day)),
-        ) # pylint: disable=no-member
+        )  # pylint: disable=no-member
 
         self.logger.info('Found {0} calendar entries'.format(len(items)))
         return [self._format_item(item) for item in items]
