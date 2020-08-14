f6d70b3ad143ba38cd0afc5a4c23e43919bef725
jamesdreid <james.reid@sagenet.com>
2020-08-14 07:50:13 -0500

    Corrected stored_date log format

--- sensors/item_sensor.py
+++ sensors/item_sensor.py
@@ -42,7 +42,7 @@ class ItemSensor(PollingSensor):
 
     def poll(self):
         stored_date = self._get_last_date()
-        self._logger.info("Stored Date: {}".format(print(stored_date)))
+        self._logger.info("Stored Date: {}".format(stored_date))
         if not stored_date:
             stored_date = datetime.now()
         start_date = self._timezone.localize(EWSDateTime.from_datetime(stored_date))
