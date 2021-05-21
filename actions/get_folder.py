d74bc2506a7f28c638d661db5fb0d712d719eaf0
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-05-21 11:32:05 +1000

    Use the new API for folders.

--- actions/get_folder.py
+++ actions/get_folder.py
@@ -4,6 +4,6 @@ from base import folder_to_dict
 
 class GetFolderAction(BaseExchangeAction):
     def run(self, folder_name):
-        result = self.account.root.get_folder_by_name(folder_name)
+        result = self.account.root / folder_name
 
         return folder_to_dict(result)
--- actions/search_items.py
+++ actions/search_items.py
@@ -4,7 +4,7 @@ from base import item_to_dict
 
 class SearchItemsAction(BaseExchangeAction):
     def run(self, folder, include_body, subject=None):
-        folder = self.account.root.get_folder_by_name(folder)
+        folder = self.account.root / folder
         if subject:
             items = folder.filter(subject__contains=subject)
         else:
