8640d94586e8ed0fe465714881bc73dd6bd797b8
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-05-21 13:48:45 +1000

    Cover the other test paths...

--- tests/list_folders_action_test_case.py
+++ tests/list_folders_action_test_case.py
@@ -5,7 +5,12 @@ from actions.list_folders import ListFoldersAction
 class ListFoldersActionTestCase(ExchangeBaseActionTestCase):
     action_cls = ListFoldersAction
 
-    def test_run_get_test_folder(self):
+    def test_default_root(self):
         result = self.get_action_instance(config=self._test_config).run()
+        expected = {"id": 1234}
+        self.assertEqual(result, expected)
+
+    def test_unique_root(self):
+        result = self.get_action_instance(config=self._test_config).run(root="foo")
         expected = {"id": 1234}
         self.assertEqual(result, expected)
--- tests/search_items_test_case.py
+++ tests/search_items_test_case.py
@@ -5,9 +5,16 @@ from actions.search_items import SearchItemsAction
 class SearchItemsActionTestCase(ExchangeBaseActionTestCase):
     action_cls = SearchItemsAction
 
-    def test_run_get_test_folder(self):
+    def test_search(self):
         result = self.get_action_instance(config=self._test_config).run(
             folder="TestFolder", subject="testing"
+        )
+        expected = {"id": 1234}
+        self.assertEqual(result, expected)
+
+    def test_search_no_subject(self):
+        result = self.get_action_instance(config=self._test_config).run(
+            folder="TestFolder"
         )
         expected = {"id": 1234}
         self.assertEqual(result, expected)
