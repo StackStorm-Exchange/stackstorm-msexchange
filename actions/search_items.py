c93d182e5fc54e2d0f9cdf02238b3b9eaea160a4
Tim Jones <jonest@sagenet.com>
2021-06-25 10:42:07 -0500

    Add support for searching for items by date using free-format date via dateutil library for parsing.

--- actions/search_items.py
+++ actions/search_items.py
@@ -3,12 +3,43 @@ from base import item_to_dict
 
 
 class SearchItemsAction(BaseExchangeAction):
-    def run(self, folder, include_body, subject=None):
+    def run(self, folder, include_body, subject=None, search_start_date=None):
         folder = self.account.root.get_folder_by_name(folder)
+
+        start_date = None
+        if search_start_date:
+            start_date = self._get_date_from_string(search_start_date)
+
         if subject:
-            items = folder.filter(subject__contains=subject)
+            if start_date:
+                items = folder.filter(subject__contains=subject,
+                                    start__gte=start_date)
+            else:
+                items = folder.filter(subject__contains=subject)
         else:
-            items = folder.all()
-        for item in items:
-            self.logger.debug("Item class: ".format(type(item)))
+            if start_date:
+                items = folder.filter(start__gte=start_date)
+            else:
+                items = folder.all()
+
         return [item_to_dict(item, include_body=include_body) for item in items]
+
+    def _get_date_from_string(self, date_str):
+        """
+        Use dateutil library (https://dateutil.readthedocs.io/) to parse
+        unstructured date string to standard format.
+        :param date_str str: Date as string in unknown/unstructured format
+        :returns datetime or None
+        """
+        try:
+            from dateutil import parser
+            start_date = parser.parse(date_str)
+        except ImportError:
+            self.logger.error("Unable to find/load 'dateutil' library.")
+            start_date = None
+        except ValueError:
+            self.logger.error("Invalid format for date input: {dt}"
+                .format(dt=date_str))
+            start_date = None
+
+        return start_date
