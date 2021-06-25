67282594405d5fefbb71d71950a33c00f5257d89
Tim Jones <jonest@sagenet.com>
2021-06-25 07:00:31 -0500

    Add handler for pack attachment configuration parameters.

--- actions/base/action.py
+++ actions/base/action.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 import os
 
 from st2common.runners.base_action import Action
+from st2common.content import utils as content_utils
 from st2client.client import Client
 from st2client.models import KeyValuePair
 from exchangelib import Account, ServiceAccount, Configuration, DELEGATE, EWSTimeZone
@@ -54,6 +55,9 @@ class BaseExchangeAction(Action):
                     access_type=DELEGATE)
             self._store_cache_configuration()
 
+        # Configure attachment parameters
+        self._attachment_configuration(config)
+
     def _store_cache_configuration(self):
         ews_url = self.account.protocol.service_endpoint
         ews_auth_type = self.account.protocol.auth_type
@@ -79,3 +83,26 @@ class BaseExchangeAction(Action):
                 primary_smtp_address=primary_smtp_address.value)
         else:
             return None
+
+    def _attachment_configuration(self, config):
+        attach_dir = config.get("attachment_directory", None)
+        if not attach_dir:
+            pack_name = getattr(self.action_service._action_wrapper,
+                                "_pack", "unknown")
+            pack_path = content_utils.get_pack_base_path(pack_name)
+            attach_dir = os.path.join(pack_path, "attachments")
+        else:
+            attach_dir = os.path.abspath(attach_dir)
+
+        # Create the folder/directory, if it doesn't exist,
+        # and make it writeable.
+        if not os.path.exists(attach_dir):
+            os.makedirs(attach_dir, exist_ok=True)
+            os.chmod(attach_dir, 0o755)
+            self.logger.info("Created directory '{dir}' and made writeable."
+                .format(dir=attach_dir))
+
+        self._attachment_directory = attach_dir
+        self._attachment_directory_max_size = config.get(
+            "attachment_directory_maximum_size", 50)
+        self._attachment_days_to_keep = config.get("attachment_days_to_keep", 7)
