1059b6139e3cd4ebf3c9dfb9dccb1f5d7cbb3f77
Tim Jones <jonest@sagenet.com>
2021-06-25 17:54:05 -0500

    Re-factor attachment save functionality to obtain results via search.

--- actions/save_attachments.py
+++ actions/save_attachments.py
@@ -1,5 +1,6 @@
 import os.path
 import random, string
+from base import item_to_dict
 from base.action import BaseExchangeAction
 from exchangelib import Message, FileAttachment, EWSDateTime
 
@@ -11,27 +12,39 @@ ATTACHMENT_FORMAT = dict([
 # Buffer size for writing attachments to file system.
 BUFFER_SIZE = 1024
 
+
 class SaveFileAttachmentAction(BaseExchangeAction):
     """
     Action to save *file* attachments from MS Exchange *email* messages.
     """
-    def run(self, message_id, changekey_id, attachment_format="BINARY"):
+    def run(self, folder="Inbox", subject=None, search_start_date=None,
+            attachment_format="BINARY"):
         """
         Action entrypoint
-        :param message_id str: MS Exchange email message ID.
-            Typically obtained from "search_items" action.
+        :param folder str: MS Exchange folder to search for messages.
+        :param subject str: [Optional] Partial, case-sensitive string to
+            search for in "Subject" field.
+        :param search_start_date str: [Optional] Date, preferably in ISO 8601
+            format, as start date for search.
         :param attachment_format str: Format to save attachments in.
             BINARY or TEXT
+
+        :returns list: List of *dictionaries* of:
+            Email Subject
+            List of fully-qualified file/path names of saved attachments
         """
 
-        attachment_filename_list = list()
-        attachment_filename_list = self._save_attachments(message_id=message_id,
-                                        changekey_id=changekey_id,
+        messages, messages_as_dicts = self._get_messages(folder=folder,
+                                        subject=subject,
+                                        search_start_date=search_start_date)
+        self.logger.debug("Messages found: \n{m}".format(m=messages_as_dicts))
+
+        attachment_result_list = self._save_attachments(messages=messages,
                                         attachment_format=attachment_format)
 
-        return attachment_filename_list
+        return attachment_result_list
 
-    def _save_attachments(self, message_id, changekey_id, attachment_format):
+    def _save_attachments(self, messages, attachment_format):
         """
         Save attachments to specified server folder from provided list of
         email messages.
@@ -39,19 +52,7 @@ class SaveFileAttachmentAction(BaseExchangeAction):
         output_format = ATTACHMENT_FORMAT[attachment_format]
         att_filename_list = list()
 
-        folder = self.account.root.get_folder_by_name("Inbox")
-        inbox_message = folder.get(id=message_id)
-        self.logger.debug("inbox_message: {ibm}".format(ibm=inbox_message))
-
-        # Get email by *combination* of message ID and changekey ID.
-        # The "account.fetch()" method returns an iterator of items.
-        # So even though we expect only one result, we must iterate.
-        item_iter = self.account.fetch(ids=list((message_id, changekey_id)))
-        self.logger.debug("item_iter, type(item_iter): {ii}, {tii}"
-            .format(ii=item_iter, tii=type(item_iter)))
-        messages = [item for item in item_iter if isinstance(item, Message)]
-        self.logger.debug("Messages retrieved: {msg}".format(msg=messages))
-        for message in item_iter:
+        for message in messages:
             # Only *email* messages are handled.
             if not isinstance(message, Message):
                 err_msg = ("Message ID '{id}' is not an email message "
@@ -119,3 +120,77 @@ class SaveFileAttachmentAction(BaseExchangeAction):
             output_filename = os.path.join(save_dir, file_name)
             if not os.path.exists(output_filename):
                 return output_filename
+
+    def _get_messages(self, folder, subject, search_start_date):
+        folder = self.account.root.get_folder_by_name(folder)
+
+        start_date = None
+        if search_start_date:
+            start_date = self._get_date_from_string(search_start_date)
+            # For email messages, MS Exchange does not support using only a
+            # start date for searches. Instead, we must use a *range* of dates
+            # for search, so we set the *end* of range to "now".
+            # See https://stackoverflow.com/a/48742644 for details.
+            end_date = self._get_date_from_string()
+
+        if subject:
+            if start_date:
+                # Try searching for *email* messages.
+                try:
+                    items = folder.filter(subject__contains=subject,
+                                            datetime_received__range=(
+                                                start_date, end_date
+                                            ))
+                except Exception:
+                    self.logger.info("No *email* messages for search criteria.")
+            else:
+                items = folder.filter(subject__contains=subject)
+        else:
+            if start_date:
+                try:
+                    items = folder.filter(datetime_received__range=(
+                                                start_date, end_date
+                                            ))
+                except Exception:
+                    self.logger.info("No *email* messages for search criteria.")
+            else:
+                items = folder.all()
+
+        return (items, [item_to_dict(item, include_body=False,
+                            folder_name=folder.name) for item in items])
+
+    # def _get_date_from_string(self, date_str=None):
+    #     """
+    #     Use dateutil library (https://dateutil.readthedocs.io/) to parse
+    #     unstructured date string to standard format.
+    #     :param date_str str: Date as string in unknown/unstructured format
+    #     :returns EWSDateTime object or None
+    #     """
+    #     # If date_str is not provided, we assume that this is for the *end*
+    #     # of the filter range, which we set to "now", using timezone from
+    #     # pack configuration.
+    #     if not date_str:
+    #         return EWSDateTime.now(tz=self.timezone)
+
+    #     try:
+    #         from dateutil import parser
+    #         import pytz
+    #         parsed_date = parser.parse(date_str)
+    #         utc_date = pytz.utc.localize(parsed_date)
+    #         local_date = utc_date
+    #         try:
+    #             local_date = utc_date.astimezone(self.timezone)
+    #         except Exception:
+    #             self.logger.error("Unable to convert search date to pack "
+    #                 "timezone. Using UTC...")
+    #         start_date = EWSDateTime.from_datetime(local_date)
+    #         self.logger.debug("Search start date: {dt}".format(dt=start_date))
+    #     except ImportError:
+    #         self.logger.error("Unable to find/load 'dateutil' library.")
+    #         start_date = None
+    #     except ValueError:
+    #         self.logger.error("Invalid format for date input: {dt}"
+    #             .format(dt=date_str))
+    #         start_date = None
+
+    #     return start_date
