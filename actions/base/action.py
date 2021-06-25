fc8aa74e846ca06009c3cfbdb324237d141bf1c4
Tim Jones <jonest@sagenet.com>
2021-06-25 13:35:58 -0500

    Re-enable all functionality for _attachment_configuration.

--- actions/base/action.py
+++ actions/base/action.py
@@ -84,36 +84,34 @@ class BaseExchangeAction(Action):
             return None
 
     def _attachment_configuration(self):
-        #self.logger.debug("type(config): {t}".format(t=type(config)))
         attach_dir = self.config.get("attachment_directory", None)
-        # if not attach_dir:
-        #     try:
-        #         from st2common.content import utils as content_utils
-        #         pack_name = getattr(self.action_service._action_wrapper,
-        #                             "_pack", "unknown")
-        #         pack_path = content_utils.get_pack_base_path(pack_name)
-        #         attach_dir = os.path.join(pack_path, "attachments")
-        #     except ImportError:
-        #         err_msg = ("Unable load import 'st2common.content.utils' "
-        #             "library. Using pack default attachment directory of "
-        #             "'/opt/stackstorm/packs/msexchange/attachments'.")
-        #         self.logger.error(err_msg)
-        #         attach_dir = "/opt/stackstorm/packs/msexchange/attachments"
-        # else:
-        #     attach_dir = os.path.abspath(attach_dir)
-        attach_dir = os.path.abspath(attach_dir)
+        if not attach_dir:
+            try:
+                from st2common.content import utils as content_utils
+                pack_name = getattr(self.action_service._action_wrapper,
+                                    "_pack", "unknown")
+                pack_path = content_utils.get_pack_base_path(pack_name)
+                attach_dir = os.path.join(pack_path, "attachments")
+            except ImportError:
+                err_msg = ("Unable load import 'st2common.content.utils' "
+                    "library. Using pack default attachment directory of "
+                    "'/opt/stackstorm/packs/msexchange/attachments'.")
+                self.logger.error(err_msg)
+                attach_dir = "/opt/stackstorm/packs/msexchange/attachments"
+        else:
+            attach_dir = os.path.abspath(attach_dir)
 
         # Create the folder/directory, if it doesn't exist,
         # and make it writeable.
-        # if not os.path.exists(attach_dir):
-        #     os.makedirs(attach_dir, exist_ok=True)
-        #     os.chmod(attach_dir, 0o755)
-        #     self.logger.info("Created directory '{dir}' and made writeable."
-        #         .format(dir=attach_dir))
+        if not os.path.exists(attach_dir):
+            os.makedirs(attach_dir, exist_ok=True)
+            os.chmod(attach_dir, 0o755)
+            self.logger.info("Created directory '{dir}' and made writeable."
+                .format(dir=attach_dir))
 
-        # if not os.access(attach_dir, os.W_OK):
-        #     raise OSError("Unable to write to attachment directory '{dir}'."
-        #         .format(dir=attach_dir))
+        if not os.access(attach_dir, os.W_OK):
+            raise OSError("Unable to write to attachment directory '{dir}'."
+                .format(dir=attach_dir))
 
         self.attachment_directory = attach_dir
         self.attachment_directory_maximum_size = int(self.config.get(
