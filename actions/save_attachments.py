fa8adab73d658b5f1e4186ce864cce52ec579777
Tim Jones <jonest@sagenet.com>
2021-06-25 18:37:21 -0500

    Changed to simple file writer for saving attachment.

--- actions/save_attachments.py
+++ actions/save_attachments.py
@@ -72,14 +72,16 @@ class SaveFileAttachmentAction(BaseExchangeAction):
                         attachment_sent=message.datetime_sent)
                     self.logger.debug("File attachment: {f}"
                         .format(f=output_file))
-                    # Perform buffered I/O to avoid memory issues
-                    # with large attachments.
-                    with open(output_file, output_format) \
-                        as f, attachment as fp:
-                        buffer = fp.read(BUFFER_SIZE)
-                        while buffer:
-                            f.write()
-                            buffer = fp.read(BUFFER_SIZE)
+                    with open(os.path.abspath(output_file), output_format) as f:
+                        f.write(attachment.content)
+                    # # Perform buffered I/O to avoid memory issues
+                    # # with large attachments.
+                    # with open(output_file, output_format) \
+                    #     as f, attachment as fp:
+                    #     buffer = fp.read(BUFFER_SIZE)
+                    #     while buffer:
+                    #         f.write()
+                    #         buffer = fp.read(BUFFER_SIZE)
                     self.logger.info("Saved attachment '{att_name}'."
                         .format(att_name=output_file))
                     att_filename_list.append(output_file)
